32ad7bc9a4e445cceabe7a10cd8d72364727f9e9
krojasalfaro7 <krojas.alfaro7@gmail.com>
2020-05-28 13:01:20 -0400

    INcorporandos datos y template a chat_on_the_web

--- chat_on_the_web/models/models.py
+++ chat_on_the_web/models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class chat_on_the_web(models.Model):
-#     _name = 'chat_on_the_web.chat_on_the_web'
+class chat_on_the_web(models.Model):
+    _name = 'chat_on_the_web.chat_on_the_web'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
+    name = fields.Char(string="Nombre del chatmodleo ")
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
 #
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
